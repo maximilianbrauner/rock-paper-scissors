e753c68bbf5b285451c0a63c41c45f9d9857b5dc
maximusheaven <maximilianbrauner95@gmail.com>
2023-12-28 13:40:52 +0100

    Updated game logic

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ["Rock", "Paper", "Scissor"];
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  console.log(computerChoice);
+  return computerChoice;
+}
+function playRound(playerSelection, computerSelection) {
+  const playerChoice = playerSelection.toLowerCase();
+  const computerChoice = computerSelection.toLowerCase();
+  if (playerChoice === computerChoice) {
+    return `Ìt's a tie. You both choose ${playerChoice}`;
+  }
+  switch (playerChoice) {
+    case "rock":
+      return computerChoice === "scissors"
+        ? "You win! Rock beats Scissors"
+        : "You lose! Paper beats Rock";
+    case "paper":
+      return computerChoice === "rock"
+        ? "You win! Paper beats Rock"
+        : "You lose! Scissors beats Paper";
+    case "scissors":
+      return computerChoice === "paper"
+        ? "You win! Scissors beats Paper"
+        : "You lose! Rock beats Scissors";
+  }
+}
+function game() {
+  userChoice = prompt("What's your input?").toLowerCase();
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound(userChoice, getComputerChoice()));
+  }
 }
