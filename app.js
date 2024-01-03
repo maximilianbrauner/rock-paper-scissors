e0d0f3846fd28d27b7434921db5b0d1ba400d9de
maximusheaven <maximilianbrauner95@gmail.com>
2024-01-03 14:29:26 +0100

    Added a play again button and tie counter since there are a lot of ties

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 let playerScore = 0;
 let computerScore = 0;
 let tieCounter = 0;
+
 const div = document.createElement("div");
 const scoreDiv = document.createElement("div");
+const finalScore = document.createElement("div");
+const finalBtn = document.createElement("href");
 const resultDiv = document.querySelector(".result");
+
 div.setAttribute(
   "style",
-  "display: flex; justify-content: center; align-items: center; font-size: 28px; padding: 15px;"
+  "display: flex; flex-direction: column; justify-content: center; align-items: center; font-size: 28px; gap: 15px;"
 );
 scoreDiv.setAttribute(
   "style",
   "display: flex; justify-content: center; align-items: center; font-size: 28px; padding: 15px;"
 );
+finalBtn.setAttribute(
+  "style",
+  "font-size: 15px; border-radius: 6px; border-style: none; color: #fca311; background: #22223b; padding: 10px; cursor: pointer;"
+);
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissor"];
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
   return computerChoice;
+}
+function updateScore() {
+  scoreDiv.textContent = `Player: ${playerScore} Computer: ${computerScore} `;
+  resultDiv.appendChild(scoreDiv);
 }
 function playRound(playerSelection) {
   computerSelection = getComputerChoice();
@@ -25,22 +37,38 @@ function playRound(playerSelection) {
     (playerSelection == "scissor" && computerSelection == "paper")
   ) {
     playerScore += 1;
-    div.textContent = `You win. ${playerSelection.slice()} beats ${computerSelection}.`;
     updateScore();
+    div.textContent = `${playerSelection.slice()} beats ${computerSelection}. you win. `;
+    if (playerScore == 5) {
+      gameOver();
+    }
   } else if (playerSelection == computerSelection) {
     tieCounter += 1;
-    div.textContent = `It's a tie.. again... You both choose ${playerSelection}`;
+    div.textContent = `it's a tie. for the ${tieCounter} time. you both choose ${playerSelection}`;
   } else {
     computerScore += 1;
-    div.textContent = `Computer wins. ${computerSelection} beats ${playerSelection}.`;
     updateScore();
+    div.textContent = `${computerSelection} beats ${playerSelection}. computer wins. `;
+    if (computerScore == 5) {
+      gameOver();
+    }
   }
   resultDiv.appendChild(div);
 }
-function updateScore() {
-  scoreDiv.textContent = `Player score: ${playerScore} Computer score: ${computerScore} `;
-  resultDiv.appendChild(scoreDiv);
+
+function gameOver() {
+  finalScore.textContent = `the game is over. the final score is ${playerScore}:${computerScore}`;
+  finalBtn.textContent = "Play again!";
+  div.appendChild(finalScore);
+  div.appendChild(finalBtn);
+  button.forEach((elem) => {
+    elem.disabled = true;
+  });
+  finalBtn.addEventListener("click", () => {
+    window.location.reload();
+  });
 }
+
 const button = document.querySelectorAll(".buttons");
 // querySelectorAll returns a Nodelist which is an array that's why i have to to loop over the array with forEach()
 button.forEach((inp) =>
